3e2fe07379daf86f0cce52cbdf48fc541bac419e
shenchao <shenchao28@huawei.com>
2024-01-12 17:43:26 +0800

    feat: 登录模块

--- meeting-room-backend/src/app.module.ts
+++ meeting-room-backend/src/app.module.ts
@@ -11,11 +11,24 @@ import { Permission } from './user/entities/permission.entity';
 import { RedisModule } from './redis/redis.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true, // 全局配置
 			envFilePath: 'src/.env', // 读取的配置文件
+		}),
+		JwtModule.registerAsync({
+			global: true,
+			useFactory(configService: ConfigService) {
+				return {
+					secret: configService.get('jwt_secret'),
+					signOptions: {
+						expiresIn: '30m',
+					},
+				};
+			},
+			inject: [ConfigService],
 		}),
 		TypeOrmModule.forRootAsync({
 			useFactory(configService: ConfigService) {
--- meeting-room-backend/src/user/user.controller.ts
+++ meeting-room-backend/src/user/user.controller.ts
@@ -3,6 +3,9 @@ import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { EmailService } from '@/email/email.service';
 import { RedisService } from '@/redis/redis.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
@@ -13,6 +16,12 @@ export class UserController {
 
 	@Inject(RedisService)
 	private redisService: RedisService;
+
+	@Inject(JwtService)
+	private jwtService: JwtService;
+
+	@Inject(ConfigService)
+	private configService: ConfigService;
 
 	@Post('register')
 	async register(@Body() registerUser: RegisterUserDto) {
@@ -37,5 +46,41 @@ export class UserController {
 	async initData() {
 		await this.userService.initData();
 		return 'done';
+	}
+
+	@Post('login')
+	async userLogin(@Body() loginUser: LoginUserDto) {
+		const vo = await this.userService.login(loginUser, false);
+
+		vo.accessToken = this.jwtService.sign(
+			{
+				userId: vo.userInfo.id,
+				username: vo.userInfo.username,
+				roles: vo.userInfo.roles,
+				permissions: vo.userInfo.permissions,
+			},
+			{
+				expiresIn:
+					this.configService.get('jwt_access_token_expires_time') ||
+					'30m',
+			},
+		);
+		vo.refreshToken = this.jwtService.sign(
+			{
+				userId: vo.userInfo.id,
+			},
+			{
+				expiresIn:
+					this.configService.get('jwt_refresh_token_expres_time') ||
+					'7d',
+			},
+		);
+		return vo;
+	}
+
+	@Post('admin/login')
+	async adminLogin(@Body() loginUser: LoginUserDto) {
+		const vo = await this.userService.login(loginUser, true);
+		return vo;
 	}
 }
